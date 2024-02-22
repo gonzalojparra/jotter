6e3260b68833833902e8067f332d489ffdec3496
Gonzalo Parra <gonzzaparra@gmail.com>
2024-02-22 18:57:14 -0300

    feat(layout): add navbar component with custom hook and update footer

---
+++ src/hooks/use-scroll-top.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export function useScrollTop(threshold = 10) {
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > threshold) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [threshold]);
+
+  return scroll;
+}
