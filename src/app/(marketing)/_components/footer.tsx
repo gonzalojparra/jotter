d355916e738777aa6b230196b2b4bd182fb8d566
Gonzalo Parra <gonzzaparra@gmail.com>
2024-02-22 18:45:35 -0300

    feat(core): add landing page with its components

---
+++ src/app/(marketing)/_components/footer.tsx
@@ -0,0 +1,9 @@
+import Logo from './logo';
+
+export default function Footer() {
+  return (
+    <div className='flex items-center w-full p-6 bg-background z-50'>
+      <Logo />
+    </div>
+  )
+}
---
+++ src/app/(marketing)/_components/heading.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { Button } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+
+export default function Heading() {
+  return (
+    <div className='max-w-3xl space-y-4'>
+      <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold'>Your notes, documents & plans. Unified welcome to <span className='underline'>Jotter</span>
+      </h1>
+      <h3 className='text-base sm:text-xl md:text-2xl font-medium'>
+        Jotter is the connected workspace where <br/> better, faster work happens.
+      </h3>
+      <Button>
+        Enter Jotter
+        <ArrowRight className='h-4 w-4 ml-2' />
+      </Button>
+    </div>
+  )
+}
