29eddd973023ddb31fb2b02784ce0a9e821627d7
Gonzalo Parra <gonzzaparra@gmail.com>
2024-02-28 13:31:36 -0300

    feat(document): add navbar into navigation

---
+++ src/app/(main)/_components/navbar.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { useQuery } from 'convex/react';
+import { useParams } from 'next/navigation';
+
+import { api } from '@/../convex/_generated/api';
+import { Id } from '@/../convex/_generated/dataModel';
+
+import { MenuIcon } from 'lucide-react';
+
+interface NavbarProps {
+  isCollapsed: boolean;
+  onResetWidth: () => void;
+};
+
+export default function Navbar({
+  isCollapsed,
+  onResetWidth
+}: NavbarProps) {
+  const params = useParams();
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId as Id<'documents'>
+  });
+
+  if (document === undefined) {
+    return <p>Loading</p>
+  }
+
+  if (document === null) {
+    return null;
+  }
+
+  return (
+    <nav className='bg-background dark:bg-[#1F1F1F] px-3 py-1 w-full flex items-center gap-x-4'>
+      {isCollapsed && (
+        <MenuIcon
+          role='button'
+          className='w-6 h-6 text-muted-foreground'
+          onClick={onResetWidth}
+        />
+        )}
+        <div className='flex items-center justify-between w-full'>
+          Navbar
+        </div>
+    </nav>
+  )
+}
