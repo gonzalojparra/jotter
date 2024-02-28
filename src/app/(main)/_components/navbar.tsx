87ae754f514dcc4eba4cbd77e1c0f9a2c59be326
Gonzalo Parra <gonzzaparra@gmail.com>
2024-02-28 14:35:27 -0300

    feat(documents): add banner when a document is in thrash

--- src/app/(main)/_components/navbar.tsx
+++ src/app/(main)/_components/navbar.tsx
@@ -7,6 +7,7 @@ import { api } from '@/../convex/_generated/api';
 import { Id } from '@/../convex/_generated/dataModel';
 
 import Title from './title';
+import Banner from './banner';
 import { MenuIcon } from 'lucide-react';
 
 interface NavbarProps {
@@ -36,17 +37,22 @@ export default function Navbar({
   }
 
   return (
-    <nav className='bg-background dark:bg-[#1F1F1F] px-3 py-1 w-full flex items-center gap-x-4'>
-      {isCollapsed && (
-        <MenuIcon
-          role='button'
-          className='w-6 h-6 text-muted-foreground'
-          onClick={onResetWidth}
-        />
+    <>
+      <nav className='bg-background dark:bg-[#1F1F1F] px-3 py-1 w-full flex items-center gap-x-4'>
+        {isCollapsed && (
+          <MenuIcon
+            role='button'
+            className='w-6 h-6 text-muted-foreground'
+            onClick={onResetWidth}
+          />
+        )}
+        <div className='flex items-center justify-between w-full'>
+          <Title initialData={document} />
+        </div>
+      </nav>
+      {document.isArchived && (
+        <Banner documentId={document._id} />
       )}
-      <div className='flex items-center justify-between w-full'>
-        <Title initialData={document} />
-      </div>
-    </nav>
+    </>
   )
 }
