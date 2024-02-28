8a3433e01a7191932f5a1373b67aca33917fc8a5
Gonzalo Parra <gonzzaparra@gmail.com>
2024-02-28 13:58:47 -0300

    feat(document): add enable editing and update title value

--- src/app/(main)/_components/title.tsx
+++ src/app/(main)/_components/title.tsx
@@ -20,17 +20,56 @@ export default function Title({ initialData }: TitleProps) {
   const [title, setTitle] = useState(initialData.title || 'Untitled');
   const [isEditing, setIsEditing] = useState(false);
 
+  const enableInput = () => {
+    setTitle(initialData.title);
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.setSelectionRange(0, inputRef.current.value.length);
+    }, 0);
+  };
+
+  const disableInput = () => {
+    setIsEditing(false);
+  };
+
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setTitle(e.target.value);
+    update({
+      id: initialData._id,
+      title: e.target.value || 'Untitled'
+    });
+  };
+
+  const onKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (e.key === 'Enter') {
+      disableInput();
+    }
+  };
+
   return (
     <div className='flex items-center gap-x-1'>
       {!!initialData.icon && <p>{initialData.icon}</p>}
       {isEditing ? (
-        <Input className='h-7 px-2 focus-visible:ring-transparent' />
+        <Input
+          ref={inputRef}
+          onClick={enableInput}
+          onBlur={disableInput}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          value={title}
+          className='h-7 px-2 focus-visible:ring-transparent'
+        />
       ) : (
         <Button
           className='font-normal h-auto p-1'
           variant='ghost'
           size='sm'
-          onClick={() => { }}
+          onClick={enableInput}
         >
           <span className='truncate'>
             {initialData.title}
