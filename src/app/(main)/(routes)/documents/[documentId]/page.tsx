8cce44acd1d6c349d29e10c1156806ae0154d544
Gonzalo Parra <gonzzaparra@gmail.com>
2024-02-29 19:40:07 -0300

    feat(toolbar): add emoji-picker-react package and implement icon picker in toolbar component

--- src/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ src/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -32,9 +32,10 @@ export default function DocumentIdPage({
 
   return (
     <div className='pb-40'>
+      <div className='h-[35vh]' />
       <div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
         <Toolbar initialData={document} />
-      </div>
+      di</div>
     </div>
   )
 }
---
+++ src/components/icon-picker.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import EmojiPicker, { Theme } from 'emoji-picker-react';
+import { useTheme } from 'next-themes';
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from '@/components/ui/popover';
+
+interface IconPickerProps {
+  onChange: (icon: string) => void;
+  children: React.ReactNode;
+  asChild?: boolean;
+};
+
+export function IconPicker({
+  onChange,
+  children,
+  asChild
+}: IconPickerProps) {
+  const { resolvedTheme } = useTheme();
+
+  const themeMap = {
+    'dark': Theme.DARK,
+    'light': Theme.LIGHT
+  };
+
+  // EmojiPicker has its own theme, so we need to map it to the next-themes theme
+  const currentTheme = (resolvedTheme || 'light') as keyof typeof themeMap
+
+  const theme = themeMap[currentTheme];
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild={asChild}>
+        {children}
+      </PopoverTrigger>
+      <PopoverContent className='p-0 w-full border-none shadow-none'>
+        <EmojiPicker
+          height={350}
+          theme={theme}
+          onEmojiClick={(data) => onChange(data.emoji)}
+        />
+      </PopoverContent>
+    </Popover>
+  )
+
+}
