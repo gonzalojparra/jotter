672c7eb3acbcebeff4f9d633c28b442f0147c88f
Gonzalo Parra <gonzzaparra@gmail.com>
2024-03-01 13:53:36 -0300

    feat(cover-image): add buttons for changing and removing cover

--- src/components/cover-image.tsx
+++ src/components/cover-image.tsx
@@ -1,7 +1,12 @@
 'use client'
 
-import { cn } from '@/lib/utils';
 import Image from 'next/image';
+import { useCoverImage } from '@/hooks/use-cover-image';
+
+import { cn } from '@/lib/utils';
+
+import { Button } from '@/components/ui/button';
+import { ImageIcon, X } from 'lucide-react';
 
 interface CoverImageProps {
   url?: string;
@@ -12,6 +17,8 @@ export function CoverImage({
   url,
   preview
 }: CoverImageProps) {
+  const coverImage = useCoverImage();
+
   return (
     <div className={cn(
       'relative w-full h-[35vh] group',
@@ -25,6 +32,28 @@ export function CoverImage({
           alt='Cover Image'
           className='object-cover'
         />
+      )}
+      {url && !preview && (
+        <div className='opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2'>
+          <Button
+            variant='outline'
+            size='sm'
+            onClick={coverImage.onOpen}
+            className='text-muted-foreground text-xs'
+          >
+            <ImageIcon className='h-4 w-4 mr-2' />
+            Change cover
+          </Button>
+          <Button
+            variant='outline'
+            size='sm'
+            onClick={() => { }}
+            className='text-muted-foreground text-xs'
+          >
+            <X className='h-4 w-4 mr-2' />
+            Remove
+          </Button>
+        </div>
       )}
     </div>
   )
