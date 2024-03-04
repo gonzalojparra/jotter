1b06ee658ffa820a4b10b63b2322bc5334fb802a
Gonzalo Parra <gonzzaparra@gmail.com>
2024-03-04 14:28:07 -0300

    feat(editor): add handle upload image functionality

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -4,6 +4,8 @@ import { BlockNoteEditor, PartialBlock } from '@blocknote/core';
 import { BlockNoteView, useBlockNote } from '@blocknote/react';
 import { useTheme } from 'next-themes';
 
+import { useEdgeStore } from '@/lib/edgestore';
+
 import '@blocknote/react/style.css';
 
 interface EditorProps {
@@ -18,8 +20,17 @@ export function Editor({
   onChange
 }: EditorProps) {
   const { resolvedTheme } = useTheme();
-  
+  const { edgestore } = useEdgeStore();
+
   const theme = resolvedTheme === 'dark' ? 'dark' : 'light';
+
+  const handleUpload = async (file: File) => {
+    const res = await edgestore.publicFiles.upload({
+      file
+    });
+
+    return res.url;
+  };
 
   const editor: BlockNoteEditor = useBlockNote({
     editable,
@@ -29,7 +40,8 @@ export function Editor({
       : undefined,
     onEditorContentChange: (editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
-    }
+    },
+    uploadFile: handleUpload
   });
 
   return (
